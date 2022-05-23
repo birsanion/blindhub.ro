005dafec98f9cb820c18c5cef7756bae7bc20e6c
Ion Birsan <johnny@smarty-app.ro>
2022-05-23 18:52:15 +0300

    Add new migration

---
+++ system/migrations/20220523185902_blindhub_angajati_locurisalvate.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Migration Task class.
+ */
+class BlindhubAngajatiLocurisalvate
+{
+    public function preUp()
+    {
+        // add the pre-migration code here
+    }
+
+    public function postUp()
+    {
+        // add the post-migration code here
+    }
+
+    public function preDown()
+    {
+        // add the pre-migration code here
+    }
+
+    public function postDown()
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Return the SQL statements for the Up migration
+     *
+     * @return string The SQL string to execute for the Up migration.
+     */
+    public function getUpSQL()
+    {
+        return <<<END
+            CREATE TABLE `qwf_angajati_locurisalvate` (
+                `idx` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                `idxauthangajat` int(10) unsigned NOT NULL,
+                `idxauthangajator` int(10) unsigned NOT NULL,
+                `idxlocmunca` int(10) unsigned NOT NULL,
+                PRIMARY KEY (`idx`),
+                KEY `idxauthangajat` (`idxauthangajat`),
+                KEY `idxauthangajator` (`idxauthangajator`),
+                KEY `idxlocmunca` (`idxlocmunca`),
+                CONSTRAINT `qwf_angajati_locurisalvate_fk_1` FOREIGN KEY (`idxauthangajat`) REFERENCES `qwf_auth_users` (`idx`) ON DELETE CASCADE ON UPDATE CASCADE,
+                CONSTRAINT `qwf_angajati_locurisalvate_fk_2` FOREIGN KEY (`idxauthangajator`) REFERENCES `qwf_auth_users` (`idx`) ON DELETE CASCADE ON UPDATE CASCADE,
+                CONSTRAINT `qwf_angajati_locurisalvate_fk_3` FOREIGN KEY (`idxlocmunca`) REFERENCES `qwf_locurimunca` (`idx`) ON DELETE CASCADE ON UPDATE CASCADE
+            );
+        END;
+    }
+
+    /**
+     * Return the SQL statements for the Down migration
+     *
+     * @return string The SQL string to execute for the Down migration.
+     */
+    public function getDownSQL()
+    {
+        return "DROP TABLE `qwf_angajati_locurisalvate`";
+    }
+
+}
